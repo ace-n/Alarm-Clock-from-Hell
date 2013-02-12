861bc78d1d7af15d2defef451b82831589c02df4
Ace Nassri <aessa_nassri@hotmail.com>
2013-02-12 01:50:40 -0600

    First commit

---
+++ AlarmWarning/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class txtMyPass
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bW1 = New System.ComponentModel.BackgroundWorker()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPass2 = New System.Windows.Forms.TextBox()
+        Me.txtPass3 = New System.Windows.Forms.TextBox()
+        Me.txtPass1 = New System.Windows.Forms.TextBox()
+        Me.lblPass1 = New System.Windows.Forms.Label()
+        Me.lblPass2 = New System.Windows.Forms.Label()
+        Me.lblPass3 = New System.Windows.Forms.Label()
+        Me.txtMyPassword = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'bW1
+        '
+        Me.bW1.WorkerReportsProgress = True
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.BackColor = System.Drawing.Color.Transparent
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!)
+        Me.lblTime.ForeColor = System.Drawing.Color.Red
+        Me.lblTime.Location = New System.Drawing.Point(78, 74)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(351, 76)
+        Me.lblTime.TabIndex = 1
+        Me.lblTime.Text = "Alarm time"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(389, 154)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(40, 41)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(78, 258)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(382, 76)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password 1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(567, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(382, 76)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Password 2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(567, 258)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(382, 76)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Password 3"
+        '
+        'txtPass2
+        '
+        Me.txtPass2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass2.Location = New System.Drawing.Point(580, 147)
+        Me.txtPass2.Name = "txtPass2"
+        Me.txtPass2.Size = New System.Drawing.Size(350, 41)
+        Me.txtPass2.TabIndex = 8
+        '
+        'txtPass3
+        '
+        Me.txtPass3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass3.Location = New System.Drawing.Point(580, 337)
+        Me.txtPass3.Name = "txtPass3"
+        Me.txtPass3.Size = New System.Drawing.Size(350, 41)
+        Me.txtPass3.TabIndex = 9
+        '
+        'txtPass1
+        '
+        Me.txtPass1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass1.Location = New System.Drawing.Point(91, 337)
+        Me.txtPass1.Name = "txtPass1"
+        Me.txtPass1.Size = New System.Drawing.Size(338, 41)
+        Me.txtPass1.TabIndex = 10
+        '
+        'lblPass1
+        '
+        Me.lblPass1.AutoSize = True
+        Me.lblPass1.Location = New System.Drawing.Point(91, 385)
+        Me.lblPass1.Name = "lblPass1"
+        Me.lblPass1.Size = New System.Drawing.Size(83, 17)
+        Me.lblPass1.TabIndex = 11
+        Me.lblPass1.Text = "Password is"
+        '
+        'lblPass2
+        '
+        Me.lblPass2.AutoSize = True
+        Me.lblPass2.Location = New System.Drawing.Point(577, 191)
+        Me.lblPass2.Name = "lblPass2"
+        Me.lblPass2.Size = New System.Drawing.Size(83, 17)
+        Me.lblPass2.TabIndex = 12
+        Me.lblPass2.Text = "Password is"
+        '
+        'lblPass3
+        '
+        Me.lblPass3.AutoSize = True
+        Me.lblPass3.Location = New System.Drawing.Point(577, 385)
+        Me.lblPass3.Name = "lblPass3"
+        Me.lblPass3.Size = New System.Drawing.Size(83, 17)
+        Me.lblPass3.TabIndex = 13
+        Me.lblPass3.Text = "Password is"
+        '
+        'txtMyPassword
+        '
+        Me.txtMyPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMyPassword.Location = New System.Drawing.Point(252, 510)
+        Me.txtMyPassword.Name = "txtMyPassword"
+        Me.txtMyPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtMyPassword.Size = New System.Drawing.Size(513, 41)
+        Me.txtMyPassword.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!)
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(239, 431)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(550, 76)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Master Password"
+        '
+        'txtTime
+        '
+        Me.txtTime.Location = New System.Drawing.Point(194, 147)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(100, 22)
+        Me.txtTime.TabIndex = 17
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(91, 147)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 17)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Time (military)"
+        '
+        'txtMyPass
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1054, 585)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTime)
+        Me.Controls.Add(Me.txtMyPassword)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblPass3)
+        Me.Controls.Add(Me.lblPass2)
+        Me.Controls.Add(Me.lblPass1)
+        Me.Controls.Add(Me.txtPass1)
+        Me.Controls.Add(Me.txtPass3)
+        Me.Controls.Add(Me.txtPass2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblTime)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "txtMyPass"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents bW1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPass2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPass3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPass1 As System.Windows.Forms.TextBox
+    Friend WithEvents lblPass1 As System.Windows.Forms.Label
+    Friend WithEvents lblPass2 As System.Windows.Forms.Label
+    Friend WithEvents lblPass3 As System.Windows.Forms.Label
+    Friend WithEvents txtMyPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtTime As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ AlarmWarning/Form1.vb
@@ -0,0 +1,250 @@
+﻿Imports System.ComponentModel
+Imports System.Text.RegularExpressions
+
+Public Class txtMyPass
+
+    Declare Function Sleep Lib "kernel32" (ByVal dwMilliseconds As Integer) As Integer
+
+    Public Msg As String = "Set alarm!"
+    Public BlockCloses As Boolean = True ' If TRUE, window-closing requests are blocked
+    Public HasBeenCloseMsgd As Boolean = False ' Allows the program to detect shutdown requests (because it will be requested to close for the first time)
+
+    ' Password generator stuff
+    Public Rand As New Random
+    Public RandomStr As String = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ1234567890" ' Password search space
+
+    ' The passwords themselves
+    Public SizeInt As Integer = 10
+    Public Password1 As String = RandomString(Rand.Next(SizeInt, SizeInt + 5))
+    Public Password2 As String = RandomString(Rand.Next(SizeInt, SizeInt + 5))
+    Public Password3 As String = RandomString(Rand.Next(SizeInt, SizeInt + 5))
+
+    ' Path of sound
+    Public SoundPath As String = "C:\Users\Ace\My Code\VB.NET\alarm_tone.wav"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ' Generate random passwords
+        lblPass1.Text = "Password is " & Password1
+        lblPass2.Text = "Password is " & Password2
+        lblPass3.Text = "Password is " & Password3
+
+        ' Generate striped image
+        Dim PerLineOffset As Integer = 1
+        Dim LineSize As Integer = 200
+
+        Dim ScrSz As Size = Screen.PrimaryScreen.WorkingArea.Size ' Screen size
+        Dim BMP As New Bitmap(ScrSz.Width, ScrSz.Height)
+        Dim BMPGr As Graphics = Graphics.FromImage(BMP)
+        BMPGr.FillRectangle(Brushes.Yellow, New Rectangle(0, 0, BMP.Width, BMP.Height))
+
+        For y = 0 To BMP.Height - 1
+            For x = ((PerLineOffset * y) Mod LineSize * 2) - LineSize * 2 To BMP.Width - 1 Step LineSize * 2
+
+                ' Skip/clamp X's that are out of range
+                If x + LineSize < 0 Then ' Skip (because the entire proposed line is out of the picture)
+                    Continue For
+                ElseIf x < 0 Then ' Clamp (because x+LineSize is still in range, so a line will be drawn)
+                    BMPGr.DrawLine(Pens.Black, New Point(0, y), New Point(x + LineSize, y))
+                Else ' No clamping required
+                    BMPGr.DrawLine(Pens.Black, New Point(x, y), New Point(x + LineSize, y))
+                End If
+
+            Next
+        Next
+
+        ' Update background
+        'Me.BackgroundImage = BMP
+
+        ' Set window position/size
+        Me.Location = New Point
+        Me.Size = ScrSz
+
+
+    End Sub
+
+    ' Message interceptor - prevents window-close messages from doing anything
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+
+        ' Shoot the message down, if appropriate
+        If BlockCloses Then
+            If m.Msg = 16 Then ' Close window
+
+                ' If first request, start closing process
+                If Not HasBeenCloseMsgd Then
+                    Me.Visible = True
+                    bW1.RunWorkerAsync()
+                    HasBeenCloseMsgd = True
+                End If
+
+                Exit Sub
+            ElseIf m.Msg = 3 Then ' Move
+                Exit Sub
+            End If
+        End If
+
+        ' Pass the message on
+        MyBase.WndProc(m)
+        Return
+
+    End Sub
+
+#Region "Commented out stuff"
+    'Private Sub bW1_DoWork() Handles bW1.DoWork
+
+    '    Sleep(100)
+
+    '    ' Keep track of seconds
+    '    For i = 30 To -1 Step -1
+    '        bW1.ReportProgress(i)
+    '        Sleep(1000)
+    '    Next
+
+    'End Sub
+
+    'Private Sub bW1_Receiver(ByVal sender As System.Object, ByVal e As ProgressChangedEventArgs) Handles bW1.ProgressChanged
+
+    '    ' Set label
+    '    lblTime.Text = "This window will close in " & e.ProgressPercentage & " seconds."
+
+    '    ' Close window if appropriate
+    '    If e.ProgressPercentage = -1 Then
+    '        BlockCloses = False
+    '        Sleep(100)
+    '        Me.Close()
+    '    End If
+
+    'End Sub
+#End Region
+
+    ' Password generator
+    Public Function RandomString(ByVal Length As Integer)
+
+        Dim Str As String = ""
+        While Str.Length < Length
+            Str &= RandomStr.Chars(Rand.Next(0, RandomStr.Length))
+        End While
+
+        Return Str
+
+    End Function
+
+    ' === Textbox freezing (password boxes are frozen when correct password is typed) ===
+    Private Sub txtPass1_TextChanged() Handles txtPass1.TextChanged
+
+        ' Pass-match check
+        If txtPass1.Text = Password1 Then
+            txtPass1.Enabled = False
+        End If
+
+        ' Close check
+        CheckClose()
+
+    End Sub
+    Private Sub txtPass2_TextChanged() Handles txtPass2.TextChanged
+
+        ' Pass-match check
+        If txtPass2.Text = Password2 Then
+            txtPass2.Enabled = False
+        End If
+
+        ' Close check
+        CheckClose()
+
+    End Sub
+    Private Sub txtPass3_TextChanged() Handles txtPass3.TextChanged
+
+        ' Pass-match check
+        If txtPass3.Text = Password3 Then
+            txtPass3.Enabled = False
+        End If
+
+        ' Close check
+        CheckClose()
+
+    End Sub
+
+    ' Close-check procedure
+    Private Sub CheckClose()
+
+        If Not (txtPass1.Enabled OrElse txtPass2.Enabled OrElse txtPass3.Enabled OrElse txtMyPassword.Enabled) Then
+            My.Computer.Audio.Stop()
+            Sleep(1000)
+            Process.GetCurrentProcess.Kill()
+        End If
+
+    End Sub
+
+    ' Hashing function
+    Private Function SimpleHash(ByVal Str As String) As Long
+
+        ' Modulo factor
+        Dim HashFactor As Integer = 16983
+
+        ' Compute hash
+        Dim Hash As Long = 0
+        For i = 0 To Str.Length - 1
+            Hash = (Hash + RandomStr.IndexOf(Str.Chars(i)) ^ (i + 1)) Mod HashFactor
+        Next
+
+        ' Return hash
+        Return Hash
+
+    End Function
+
+    Private Sub myPassChanged() Handles txtMyPassword.TextChanged
+
+        If SimpleHash(txtMyPassword.Text) = 1663 Then
+
+            ' Disable
+            txtMyPassword.Enabled = False
+
+            ' Check close
+            CheckClose()
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click() Handles Button1.Click
+
+        ' Find trigger date
+        Dim Time_H As Integer = -1
+        Dim Time_M As Integer = -1
+        If Not (Regex.IsMatch(txtTime.Text, "\d{1,2}:\d{1,2}") AndAlso _
+            Integer.TryParse(Regex.Match(txtTime.Text, "\d{1,2}").Value, Time_H) AndAlso _
+            Integer.TryParse(Regex.Match(txtTime.Text, "(?<=(:))\d{1,2}").Value, Time_M)) Then
+            MsgBox("Invalid time. Time must between 0 and 23:59 hours (military time). [1]")
+            Exit Sub
+        End If
+
+        ' == Input validation ==
+        If Time_H < 0 OrElse Time_H > 23 OrElse Time_M < 0 OrElse Time_M > 59 Then
+            MsgBox("Invalid time. Time must between 0 and 23:59 hours (military time). [2]")
+            Exit Sub
+        End If
+
+        ' == Wait until then ==
+        Dim Now As DateTime = My.Computer.Clock.LocalTime
+        Dim TargetTime As New DateTime(Now.Year, Now.Month, Now.Day, Time_H, Time_M, 0)
+
+        ' Day offset
+        If Now.Hour * 60 + Now.Minute > Time_H * 60 + Time_M Then
+            TargetTime.AddDays(1)
+        End If
+
+        ' Get difference
+        Dim SleepTime As TimeSpan = TargetTime - Now
+
+        ' Sleep
+        Console.WriteLine(SleepTime.TotalSeconds)
+        Sleep(SleepTime.TotalSeconds * 1000)
+
+        ' F*ck sh*t up
+        My.Computer.Audio.Play(SoundPath, AudioPlayMode.BackgroundLoop)
+
+    End Sub
+
+
+
+End Class
